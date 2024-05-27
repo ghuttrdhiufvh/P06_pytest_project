395e85375b5fbda4ffc341482da4f99760c2be9c
chunweng <23021701@myrp.edu.sg>
2024-05-27 10:36:41 +0800

    int commit

---
+++ P06_pytest_project/tests/__init__.py
@@ -0,0 +1,15 @@
+class Calculator():
+
+    def test_add(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.add(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
+
---
+++ P06_pytest_project/tests/test_sample.py
@@ -0,0 +1,8 @@
+def test_assert_true():
+    assert True
+
+def test_assert_false():
+    assert False
+
+def add(self, a, b):
+    return a + b
